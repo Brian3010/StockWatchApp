38f0459d0e3800e2b152c96e9dca429cc71fe22d
Brian <briannguyenwg@gmail.com>
2024-01-23 02:39:40 +1100

    install @headlessui/react

--- frontend/src/pages/stockHistory/index.tsx
+++ frontend/src/pages/stockHistory/index.tsx
@@ -1,3 +1,4 @@
+import { Tab } from '@headlessui/react';
 import { ChangeEventHandler, useState } from 'react';
 import { GetStockListsByDateT, getStockListsByDate } from '../../firebase';
 import { isObjectEmpty } from '../../utils/helpers';
@@ -41,6 +42,19 @@ export default function StockHistory() {
       )}
 
       {stockLists && isObjectEmpty(stockLists) && <div>Stock not submited on this day</div>}
+
+      <Tab.Group >
+        <Tab.List>
+          <Tab>Tab 1</Tab>
+          <Tab>Tab 2</Tab>
+          <Tab>Tab 3</Tab>
+        </Tab.List>
+        <Tab.Panels>
+          <Tab.Panel>Content 1</Tab.Panel>
+          <Tab.Panel>Content 2</Tab.Panel>
+          <Tab.Panel>Content 3</Tab.Panel>
+        </Tab.Panels>
+      </Tab.Group>
     </>
   );
 }
