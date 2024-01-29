7855a69df2589d79f16a6482132067adba9bcb66
Brian <briannguyenwg@gmail.com>
2024-01-29 19:48:09 +1100

    style: only display navbar in the homepage

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,11 +1,13 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 
 export default function Layout() {
+  const location = useLocation();
+  console.log({ location });
   return (
     <>
       <div className="container mx-auto max-w-screen-lg ">
-        <NavBar />
+        {location.pathname === '/' && <NavBar />}
         <main className="h-lvh bg-[#F9F9FB] px-3">
           <Outlet />
         </main>
