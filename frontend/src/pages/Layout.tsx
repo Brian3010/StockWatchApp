3aeab3d061b90b4f07560ca9a72b906b3e48509a
Brian <briannguyenwg@gmail.com>
2024-01-31 02:29:48 +1100

    style: adjust the main container width and height

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -1,22 +1,22 @@
 import { Outlet, useLocation } from 'react-router-dom';
-import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
+import NavBar from '../components/NavBar';
 
 export default function Layout() {
   const location = useLocation();
   console.log({ location });
   return (
     <>
-      <div className="relavtive container relative mx-auto flex min-h-screen max-w-screen-lg flex-col bg-gami-primary">
-        {location.pathname === '/' && <NavBar />}
-        <main
-          className={`min-h-screen bg-[#F9F9FB] px-3 pb-28 ${location.pathname === '/' && 'min-h-[calc(100vh-64px)] rounded-t-3xl'}`}
-        >
-          <Outlet />
-        </main>
-        <footer className={`bg-neutral-200 p-[1rem] ${location.pathname === '/' && 'absolute inset-x-0 bottom-0'}`}>
-          <Footer/>
-        </footer>
+      <div className="container mx-auto max-w-screen-lg lg:max-w-screen-xs h-screen">
+        <div className="relative flex flex-col h-full bg-[#F9F9FB] lg:max-h-[960px] ">
+          {location.pathname === '/' && <NavBar />}
+          <main className={`h-full px-3 pb-28 ${location.pathname === '/' ? ' rounded-t-3xl' : ''}`}>
+            <Outlet />
+          </main>
+          <footer className={`bg-neutral-200 p-[1rem] ${location.pathname === '/' && 'absolute inset-x-0 bottom-0'}`}>
+            <Footer />
+          </footer>
+        </div>
       </div>
     </>
   );
