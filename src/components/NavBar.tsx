67373a758f0c5dff6d74037438c220b6e24329b3
Brian <briannguyenwg@gmail.com>
2024-02-07 03:57:47 +1100

    feats: add reloading button to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 export default function NavBar() {
   return (
     <nav className="relative flex w-full flex-wrap items-center justify-between bg-gami-primary py-1">
-      <div className="flex w-full flex-wrap items-center justify-center">
+      <div className="relative flex w-full flex-wrap items-center justify-center">
         <div>
           <a className="mx-2 my-1 flex items-center" href="/">
             <div className="flex flex-col items-center">
@@ -21,6 +21,24 @@ export default function NavBar() {
                 </svg>
                 Gami Hawthorn
               </span>
+              <div className="absolute right-3 top-1/2 -translate-x-1/2 -translate-y-1/2 transform">
+                <button onClick={() => window.location.reload()}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="h-7 w-7 text-gami-text hover:animate-spin"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+                    />
+                  </svg>
+                </button>
+              </div>
             </div>
           </a>
         </div>
