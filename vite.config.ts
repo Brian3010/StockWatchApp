de7b1a93b9f1c7cc8429fafda0ce2d0eb07d3f00
Brian <briannguyenwg@gmail.com>
2024-03-01 00:21:58 +1100

    build: adjust vite config

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default ({ mode }) => {
 
   return defineConfig({
     plugins: [react()],
-    esbuild: process.env.PROD && {
+    esbuild: {
       drop: ['console', 'debugger'],
     },
   });
